9d508d224a489413fe0ca125e20e26945f38e200
unknown <kamen.boev@gmail.com>
2020-12-30 23:23:17 +0200

    Initial cards deal added

--- gameplay.py
+++ gameplay.py
@@ -15,10 +15,18 @@ poker_scores = {
     'straight_flush': 9 
 }
 
+test_players = [
+    ('boba', 1000),
+    ('booba', 2000),
+    ('bebop', 2000),
+    ('abba', 2000),
+    ('boabab', 2000),
+    ('bae', 1000)
+]
+
 # Declaring input variables with some initial values
-players_number = 0
-small_blind = 0
-big_blind = 0
+small_blind = 5
+big_blind = 10
 
 # Card class
 class Card:
@@ -35,12 +43,17 @@ class Card:
 
 # Player class
 class Player:
-    def __init__(self, total_cash, current_bet, hand):
-        self.total_cash: total_cash
-        self.current_bet: current_bet
-        self.hand: hand
+    def __init__(self, name, total_cash):
+        self.total_cash = total_cash
+        self.name = name
 
-# Deck 
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self):
+        return f'{self.name} ${self.total_cash}'
+
+# Init deck 
 def build_deck(card_values, card_colors):
     deck = []
     for value in card_values:
@@ -58,12 +71,44 @@ def shuffle_deck(deck):
     random.shuffle(shuffled)
     return shuffled
 
-print('Shuffled:', shuffle_deck(initial_deck))
-
-
-# Deal
-
-# Evaluate hand function
+# Init players
+def make_player(name_cash):
+    return Player(name_cash[0], name_cash[1])
+players = list(map(make_player, test_players)) # use list comprehention insted and add index
+print('players:', players)
 
 # Game loop
+players_number = len(players)
+stage = 0
+dealer_indx = 0
+bets_on_table = [0 for indx in range(players_number)]
+common_cards = []
 
+while stage != -1:
+
+    if stage == 0:
+        deck = shuffle_deck(initial_deck)
+        print('start deck', deck)
+
+        for player in players:
+            # Dealing players
+            player.hand = deck[:2] # MAYBE keep hands in separate var?
+            deck = deck[2:]
+
+            # Auto betting blinds
+            player_indx = players.index(player)
+            if player_indx == (dealer_indx + 1) % players_number:
+                bets_on_table[player_indx] += small_blind
+                player.total_cash -= small_blind
+            elif player_indx == (dealer_indx + 2) % players_number:
+                bets_on_table[player_indx] += big_blind
+                player.total_cash -= big_blind
+        
+        common_cards = deck[:3]
+        deck = deck[3:]
+    
+        print('Players', players)
+        print('bets:', bets_on_table)
+        print('common cards', common_cards)
+        print('deck', deck)
+        stage = -1
