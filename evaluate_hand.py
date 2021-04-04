7e752dedaaba42a55e232eb7f94d55e10227a5d1
unknown <kamen.boev@gmail.com>
2021-04-05 00:34:48 +0300

    Adding hand evaluation stuff

--- evaluate_hand.py
+++ evaluate_hand.py
@@ -1,7 +1,6 @@
 from collections import Counter
 from game_params import poker_scores, card_values, card_colors
 from deck import Deck
-
 from card import Card
 
 test_deck = Deck()
@@ -21,37 +20,52 @@ def get_counts(hand):
     return [card_values.count(value) for value in card_values]
 
 # Boolean checks for different hands
-def high_card(hand):
-    counts = get_counts(hand)
-    return counts.count(1) == 5
+class Hand_Checks:    
+    def high_card(self, hand):
+        counts = get_counts(hand)
+        return counts.count(1) == 5
 
-def two_pairs(hand):
-    counts = get_counts(hand)
-    return counts.count(2) == 4    
+    def two_pairs(self, hand):
+        counts = get_counts(hand)
+        return counts.count(2) == 4
 
-def n_of_a_kind(hand, n):
-    card_values = [card.value for card in hand]
-    for value in card_values:
-        if card_values.count(value) == n:
-            return True
-    return False
+    def n_of_a_kind(self, hand, n):
+        card_values = [card.value for card in hand]
+        for value in card_values:
+            if card_values.count(value) == n:
+                return True
+        return False
 
-def straight(hand):
-    straight_cards = card_values
-    if card_values[-1] == 'A':
-        straight_cards = ['A'] + straight_cards
-    indexes = [straight_cards.index(card.value) for card in hand]
-    indexes.sort()
-    counts = [indexes.count(i) for i in indexes]
-    return not (counts.count(1) != 5 or max(indexes) - min(indexes) != 4)
-    
-def full_house(hand):
-    counts = get_counts(hand)
-    return (counts.count(3) == 3 and counts.count(2) == 2)
+    def straight(self, hand):
+        straight_cards = card_values
+        if card_values[-1] == 'A':
+            straight_cards = ['A'] + straight_cards
+        indexes = [straight_cards.index(card.value) for card in hand]
+        indexes.sort()
+        counts = [indexes.count(i) for i in indexes]
+        return not (counts.count(1) != 5 or max(indexes) - min(indexes) != 4)
+        
+    def full_house(self, hand):
+        counts = get_counts(hand)
+        return (counts.count(3) == 3 and counts.count(2) == 2)
 
-# def score_hand(hand):
-#     counts = Counter(map(lambda card: card.value, hand))
-#     max_repeated = None
-    # for count in counts:
-    #     if count:
-    #         return 1
+    def pair(self, hand):
+        return self.n_of_a_kind(hand, 2)
+
+    def three_of_a_kind(self, hand):
+        return self.n_of_a_kind(hand, 3)
+
+    def four_of_a_kind(self, hand):
+        return self.n_of_a_kind(hand, 4)
+
+    def flush(self, hand):
+        colors = [card.color for card in hand]
+        return (colors.count(colors[0]) == 5)
+
+    def straight_flush(self, hand):
+        return (self.straight(hand) and self.flush(hand))
+
+
+def get_hand_max_score(hand, scores):
+    all_checks = [scores[key] if Hand_Checks[key](hand) else 0 for key in scores]
+    return max(all_checks)
